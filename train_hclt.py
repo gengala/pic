3b240edcc59164bf1d701e6d0d5fe40a503f45ad
gengala <gennarogala1396@gmail.com>
2024-01-06 22:59:24 +0100

    minors

--- train_hclt.py
+++ train_hclt.py
@@ -105,7 +105,7 @@ for train_step in range(1, args.train_steps + 1):
         with torch.no_grad():
             log_norm_const = hclt.log_norm_constant.cpu() if args.normalize else 0
             valid_lls_log.append(float(torch.cat(
-                [hclt(x.to(device=dev), has_nan=False) - log_norm_const for x in valid.split(args.batch_size)]).mean()))
+                [hclt(x.to(device=dev), has_nan=False).cpu() - log_norm_const for x in valid.split(args.batch_size)]).mean()))
         if valid_lls_log[-1] > best_valid_ll:
             best_valid_ll = valid_lls_log[-1]
             torch.save(hclt, log_dir + 'hclt.pt')
